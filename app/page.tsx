36a90db965f7065d8bce8022a4bcaa7b655b4cb5
Blackxok <nasridoni1@gmail.com>
2024-11-28 23:41:24 +0500

    add font var

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-	return <div>Home 6:54</div>
+	return (
+		<>
+			<h1 className='text-5xl font-roboto'>Assalomu alekum</h1>
+			<div className='container font-jetB'>11:53</div>
+		</>
+	)
 }
